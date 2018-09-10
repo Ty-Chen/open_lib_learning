47be07bc481e8cd07eb9dd74c1d635ebe3c62990
ty <369494003@qq.com>
2018-09-10 22:48:24 +0800

    Add notations for evhttp client

--- Libevent-master/sample/https-client.c
+++ Libevent-master/sample/https-client.c
@@ -54,6 +54,7 @@ http_request_done(struct evhttp_request *req, void *ctx)
 	char buffer[256];
 	int nread;
 
+	/*返回回复为空则说明出错，检查问题*/
 	if (req == NULL) {
 		/* If req is NULL, it means an error occurred, but
 		 * sadly we are mostly left guessing what the error
@@ -61,16 +62,22 @@ http_request_done(struct evhttp_request *req, void *ctx)
 		struct bufferevent *bev = (struct bufferevent *) ctx;
 		unsigned long oslerr;
 		int printed_err = 0;
+
+		/*打印错误码*/
 		int errcode = EVUTIL_SOCKET_ERROR();
 		fprintf(stderr, "some request failed - no idea which one though!\n");
-		/* Print out the OpenSSL error queue that libevent
+		
+		/* 查找openssl错误
+		 * Print out the OpenSSL error queue that libevent
 		 * squirreled away for us, if any. */
 		while ((oslerr = bufferevent_get_openssl_error(bev))) {
 			ERR_error_string_n(oslerr, buffer, sizeof(buffer));
 			fprintf(stderr, "%s\n", buffer);
 			printed_err = 1;
 		}
-		/* If the OpenSSL error queue was empty, maybe it was a
+		
+		/* 查找套接字错误
+		 * If the OpenSSL error queue was empty, maybe it was a
 		 * socket error; let's try printing that. */
 		if (! printed_err)
 			fprintf(stderr, "socket error = %s (%d)\n",
@@ -79,10 +86,12 @@ http_request_done(struct evhttp_request *req, void *ctx)
 		return;
 	}
 
+	/*正常回复*/
 	fprintf(stderr, "Response line: %d %s\n",
 	    evhttp_request_get_response_code(req),
 	    evhttp_request_get_response_code_line(req));
 
+	/*释放evbuffer并打印*/
 	while ((nread = evbuffer_remove(evhttp_request_get_input_buffer(req),
 		    buffer, sizeof(buffer)))
 	       > 0) {
