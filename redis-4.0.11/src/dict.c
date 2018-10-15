f13c6105fd5ccd7cbc56db2ef87c9d4c9d0b7a2f
ty <369494003@qq.com>
2018-10-15 22:01:14 +0800

    Update redis-4.0.11/src/dict.c

--- redis-4.0.11/src/dict.c
+++ redis-4.0.11/src/dict.c
@@ -365,15 +365,18 @@ int dictReplace(dict *d, void *key, void *val)
 {
     dictEntry *entry, *existing, auxentry;
 
-    /* Try to add the element. If the key
+    /* 调用dictAddRaw尝试添加key
+     * 成功则返回，失败则继续执行复写操作
+     * Try to add the element. If the key
      * does not exists dictAdd will suceed. */
-    entry = dictAddRaw(d,key,&existing);
+    entry = dictAddRaw(d, key, &existing);
     if (entry) {
         dictSetVal(d, entry, val);
         return 1;
     }
 
-    /* Set the new value and free the old one. Note that it is important
+    /* 执行到这里说明key-value键值对已存在，因此使用复写
+     * Set the new value and free the old one. Note that it is important
      * to do that in this order, as the value may just be exactly the same
      * as the previous one. In this context, think to reference counting,
      * you want to increment (set), and then decrement (free), and not the
