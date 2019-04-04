b766fb52d73ac6d7b059d10ba8a8936667d46a79
Ty-Chen <369494003@qq.com>
2019-04-04 20:47:02 +0800

    Update hyperloglog.c

--- redis-4.0.11/src/hyperloglog.c
+++ redis-4.0.11/src/hyperloglog.c
@@ -193,10 +193,10 @@ struct hllhdr {
 #define HLL_INVALIDATE_CACHE(hdr) (hdr)->card[7] |= (1<<7)
 #define HLL_VALID_CACHE(hdr) (((hdr)->card[7] & (1<<7)) == 0)
 
-#define HLL_P 14 /* The greater is P, the smaller the error. */
-#define HLL_REGISTERS (1<<HLL_P) /* With P=14, 16384 registers. */
+#define HLL_P 14 /* 分桶指数 The greater is P, the smaller the error. */
+#define HLL_REGISTERS (1<<HLL_P) /* 桶数 With P=14, 16384 registers. */
 #define HLL_P_MASK (HLL_REGISTERS-1) /* Mask to index register. */
-#define HLL_BITS 6 /* Enough to count up to 63 leading zeroes. */
+#define HLL_BITS 6 /* 6比特存储连续63个0的情况 Enough to count up to 63 leading zeroes. */
 #define HLL_REGISTER_MAX ((1<<HLL_BITS)-1)
 #define HLL_HDR_SIZE sizeof(struct hllhdr)
 #define HLL_DENSE_SIZE (HLL_HDR_SIZE+((HLL_REGISTERS*HLL_BITS+7)/8))
@@ -210,7 +210,7 @@ static char *invalid_hll_err = "-INVALIDOBJ Corrupted HLL object detected\r\n";
 /* =========================== Low level bit macros ========================= */
 
 /* Macros to access the dense representation.
- *
+ * 为了计算速度采用了宏
  * We need to get and set 6 bit counters in an array of 8 bit bytes.
  * We use macros to make sure the code is inlined since speed is critical
  * especially in order to compute the approximated cardinality in
