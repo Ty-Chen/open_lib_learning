a0d4b9b4a23ec27d0bce3a01ac3f899898c7725e
Ty-Chen <369494003@qq.com>
2019-08-15 19:40:40 +0800

    Update quicklist.c

--- redis-4.0.11/src/quicklist.c
+++ redis-4.0.11/src/quicklist.c
@@ -527,14 +527,17 @@ REDIS_STATIC int _quicklistNodeAllowInsert(const quicklistNode *node,
         return 0;
 }
 
+// 融合a和b
 REDIS_STATIC int _quicklistNodeAllowMerge(const quicklistNode *a,
                                           const quicklistNode *b,
                                           const int fill) {
     if (!a || !b)
         return 0;
 
-    /* approximate merged ziplist size (- 11 to remove one ziplist
-     * header/trailer) */
+    /* 融合后大小：需要减去一个头部11字节
+     * approximate merged ziplist size (- 11 to remove one ziplist
+     * header/trailer) 
+     */
     unsigned int merge_sz = a->sz + b->sz - 11;
     if (likely(_quicklistNodeSizeMeetsOptimizationRequirement(merge_sz, fill)))
         return 1;
